b3dd59e33705733d13a68c75681b49ceb6b1e368
Kokojam7a <Kokojam7a@DESKTOP-2JBLTON>
2020-01-29 22:05:35 +0200

    Добавьте файлы проекта.

---
+++ NumberManipulation/Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberManipulation
+{
+    class Multiplication:Plus
+    {
+        public override double pluses(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ NumberManipulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Plus Plas = new Plus();
+            Console.WriteLine(Plas.pluses(x, y));
+            Division Div = new Division();
+            Console.WriteLine(Div.pluses(x, y));
+            Multiplication Mult = new Multiplication();
+            Console.WriteLine(Mult.pluses(x, y));
+            Console.ReadKey();
+        }
+    }
+}
